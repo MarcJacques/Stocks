69095b5447be02d85148dd0649d65b25d2943615
Marc <marcjacques@me.com>
2022-04-02 13:21:51 -0400

    Add updateSearchResult features

--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -12,15 +12,30 @@ class WatchListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        setSeacrhController()
+        setSearchController()
     }
     
-    private func setSeacrhController() {
+    private func setSearchController() {
         let resultVC = SearchResultsViewController()
         let searchVC = UISearchController(searchResultsController: resultVC)
+        searchVC.searchResultsUpdater = self
         navigationItem.searchController = searchVC
     }
 
 
+}
+
+extension WatchListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              let results = searchController.searchResultsController as? SearchResultsViewController,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        //call to search
+        
+        print(query)
+    }
 }
 
