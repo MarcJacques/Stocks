447085ab210b0c89555746f80740412ff699247e
Marc <marcjacques@me.com>
2022-04-02 09:29:45 -0400

    Add Haptic Manager file

---
+++ Stocks/Managers/HapticsManager.swift
@@ -0,0 +1,21 @@
+//
+//  HapticsManager.swift
+//  Stocks
+//
+//  Created by Marc Jacques on 4/2/22.
+//
+
+import Foundation
+
+final class HapticsManager {
+    static let shared = HapticsManager()
+    
+    private init() {}
+    
+    // MARK: - Public
+    
+    public func vibrateForSelection() {
+        //Vibrate upon seleciton
+    }
+    
+}
