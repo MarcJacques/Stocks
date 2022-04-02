f3ce90e9a5b3b0efed4a8520d8e8b3b0907b7e9b
Marc <marcjacques@me.com>
2022-04-02 09:48:03 -0400

    Add search bar to watchListVC

--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -12,6 +12,13 @@ class WatchListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setSeacrhController()
+    }
+    
+    private func setSeacrhController() {
+        let resultVC = SearchResultsViewController()
+        let searchVC = UISearchController(searchResultsController: resultVC)
+        navigationItem.searchController = searchVC
     }
 
 
